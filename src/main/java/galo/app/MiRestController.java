758d057a9564aeb891651d5aece0c4a5242c2443
Gagalo <juangalofait2@gmail.com>
2025-09-22 07:51:27 -0300

    primer commit

---
+++ src/main/java/galo/app/MiRestController.java
@@ -0,0 +1,14 @@
+package galo.app;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MiRestController
+{
+	@RequestMapping("/hola")
+	public String test()
+	{
+		return "Hola Mundo!";
+	}
+}
